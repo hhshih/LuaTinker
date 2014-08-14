cd8c47913910292be09f1f219692b6325c45d4ab
Hsi-Hung Shih <hsihung.shih@digipen.edu>
2014-08-14 12:57:13 -0700

    modify personal files

---
+++ README.md
@@ -0,0 +1,1 @@
+A slightly modified version of lua_tinker for personal use.
---
+++ premake5.lua
@@ -0,0 +1,55 @@
+if (not _ACTION) then return end
+
+-- A solution contains projects, and defines the available configurations
+solution "lua_tinker"
+   configurations { "Debug", "Release" }
+   location ("build/" .. _ACTION)
+   defines {"_WINDOWS", "WIN32"}
+ 
+   -- A project defines one build target
+   project "lua_tinker"
+      targetname "lua_tinker"
+      language "C++"
+
+      -- project wise settings
+      files 
+      { 
+          "lua_tinker.cpp"
+      }
+
+      includedirs 
+      {
+          ".."
+      }
+
+      flags 
+      {
+          "Unicode", 
+          "WinMain",
+      }
+
+      defines "_CRT_SECURE_NO_WARNINGS"
+      warnings "Extra"
+
+      kind "StaticLib"
+
+      configuration "Debug*"
+         defines 
+         { 
+             "_DEBUG", 
+             "DEBUG" 
+         }
+         flags { "Symbols" }
+         optimize "Off"
+ 
+      configuration "Release*"
+         defines { "NDEBUG" }
+         optimize "Full"
+         flags { 'LinkTimeOptimization', }
+
+      for _, config in ipairs(configurations()) do
+         configuration(config)
+         targetdir ("lib/" .. config)
+         objdir ("obj/" .. config)
+      end
+
